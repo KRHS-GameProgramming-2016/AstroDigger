b757ba88efbce6f9dcbb16d31787727cf0a4067e
harrison lord <harrison.lord@kearsarge.org>
2016-11-28 11:13:15 -0500

    g

--- Level.py
+++ Level.py
@@ -1,1 +1,36 @@
+import pygame, sys, math
+from Wall import *
+class Level():
+    def __init__(self, levelFile, tileSize=50):
+        self.walls = []
+        self.players = []
+        self.ballSpawns = []
+        self.tileSize = tileSize
+        
+        self.loadLevel(levelFile)
+        
+    def unloadLevel(self): 
+        self.walls = []
+        self.players = []
+        self.ballSpawns = []
+               
+    def loadLevel(self, levelFile):        
+        f = open("rsc/levels/"+levelFile, 'r')
+        lines = f.readlines()
+        f.close()
 
+
+
+
+**************
+*            *
+*            *
+*            *
+*111111111111*
+*111111111111*
+*111111111111*
+*111111111111*
+*111111111111*
+*111111111111*
+*111111111111*
+**************
