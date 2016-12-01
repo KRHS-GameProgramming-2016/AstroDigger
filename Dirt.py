2ace50ea99182e9ea4fd1972710f580155ab8ea0
jpoltack <joseph.poltack@kearsarge.org>
2016-12-01 11:22:01 -0500

    dxgbujnrat

--- Dirt.py
+++ Dirt.py
@@ -2,11 +2,7 @@ import pygame, sys, math
 
 class Wall():
     def __init__(self, pos=[0,0], size=None):
-        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt.png")
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt.png")
         if size:
             self.image = pygame.transform.scale(self.image, [size,size])
-        self.rect = self.image.get_rect(center = pos) 
-        
-        
-    
-            
+        self.rect = self.image.get_rect(center = pos)
