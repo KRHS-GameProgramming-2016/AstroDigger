e36e00c83eaa9ebda5278d82244dba4a15405a70
Steven <steven.rayno@kearsarge.org>
2016-12-01 11:14:46 -0500

    Spooner 'helped' us(Me & Joe) with Score

--- Score.py
+++ Score.py
@@ -1,4 +1,8 @@
 import pygame, sys, math 
 
 class Score():
-    __init__(self, pos):
+    def __init__(self, pos):
+        self.value = 0
+        self.font = pygame.font.Font()
+        self.image = font.render("Score: " + str(self.value), True, (255,0,0))
+        self.rect = self.image.rect
