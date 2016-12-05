9bf0feddab26b19b5253f7f5986ed58104a6e6dd
jpoltack <joseph.poltack@kearsarge.org>
2016-12-05 11:12:05 -0500

    dirt12345

--- Dirt.py
+++ Dirt.py
@@ -2,7 +2,27 @@ import pygame, sys, math
 
 class Wall():
     def __init__(self, pos=[0,0], size=None):
-        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt.png")
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt For Level 1.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
+        self.rect = self.image.get_rect(center = pos)
+    def __init__(self, pos=[0,0], size=None):
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt For Level 2.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
+        self.rect = self.image.get_rect(center = pos)
+    def __init__(self, pos=[0,0], size=None):
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt For Level 3.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
+        self.rect = self.image.get_rect(center = pos)
+    def __init__(self, pos=[0,0], size=None):
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt For Level 4.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
+        self.rect = self.image.get_rect(center = pos)
+    def __init__(self, pos=[0,0], size=None):
+        self.image = pygame.image.load("AstroDigger/Resoucres/Dirt/Dirt For Level 5.png")
         if size:
             self.image = pygame.transform.scale(self.image, [size,size])
         self.rect = self.image.get_rect(center = pos)
