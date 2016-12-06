1a7ce09b98a03183fbf08a6b9c9b6ea1d4682c63
harrison lord <harrison.lord@kearsarge.org>
2016-12-06 11:18:31 -0500

    fbnr

--- Level.py
+++ Level.py
@@ -1,20 +1,36 @@
 import pygame, sys, math
-from Dirt import *
+from dirt import *
+from diggerlevel1 import *
 class Level():
     def __init__(self, levelFile, tileSize=64):
-        self.walls = []
+        self.dirt = []
         self.players = []
-        #self.ballSpawns = []
+        self.playerSpawns = []
         self.tileSize = tileSize
         
         self.loadLevel(levelFile)
         
     def unloadLevel(self): 
-        self.walls = []
+        self.dirt = []
         self.players = []
         self.ballSpawns = []
                
     def loadLevel(self, levelFile):        
-        f = open("Resources/Levels/" + levelFile)
+        f = open("rsc/levels/"+levelFile, 'r')
         lines = f.readlines()
         f.close()
+
+
+
+
+*            *
+*            *
+* a          *
+*111111111111*
+*111111111111*
+*11 x 1111111*
+*111111111111*
+*11111111 x 1*
+*111111111111*
+*111111111111*
+
