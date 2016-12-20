165bcf4050b3e1ad7fe7f15c20f0a3fa740de51d
StevenRayno <steven.rayno@kearsarge.org>
2016-12-20 14:30:35 -0500

    Enemies will now move on x+y axis now | Testing.py is my personal testing file :3

--- Enemy.py
+++ Enemy.py
@@ -1,4 +1,4 @@
-import pygame, sys, math
+import pygame, sys, math, random
 
 class Enemy():
     def __init__(self, image, speed=[0,0], pos=[0,0], size=64):
@@ -6,17 +6,42 @@ class Enemy():
         if size:
             self.image = pygame.transform.scale(self.image, [size, size])
         self.rect = self.image.get_rect(center = pos)
-        self.speedx = speed[0]
-        self.speedy = speed[1]
-        self.speed = [self.speedx, self.speedy]
+        self.maxSpeed = speed
+        self.decideDirection()
         self.didBounceX = False
         self.didBounceY = False
+        self.size = size
+        
         
     def move(self):
         self.didBounceX = False
         self.didBounceY = False
         self.speed = [self.speedx, self.speedy]
         self.rect = self.rect.move(self.speed)
+        if self.speedx != 0:     #moving left/right
+            if self.rect.left % self.size == 0:
+                print "left/right", self.rect.center[0]
+                self.decideDirection()
+        else:     #moving up/down
+            if self.rect.top % self.size == 0:
+                print "left/right", self.rect.center[1]
+                self.decideDirection()
+     
+    def decideDirection(self):
+        d = random.randint(0,3)
+        if d == 0: #up
+            self.speedx = 0
+            self.speedy = -self.maxSpeed
+        elif d == 1: #right
+            self.speedx = self.maxSpeed
+            self.speedy = 0
+        elif d == 2: #down
+            self.speedx = 0
+            self.speedy = self.maxSpeed
+        elif d == 3: #left
+            self.speedx = -self.maxSpeed
+            self.speedy = 0
+            
         
     def dirtCollide(self, other):
         if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
@@ -25,7 +50,7 @@ class Enemy():
                     self.speedx = -self.speedx
                 if not self.didBounceY:
                     self.speedy = -self.speedy
-    
+                    
     def screenCollide(self, screenWidth):
         if self.rect.center[0] > screenWidth:
             self.rect.center = [0, self.rect.center[1]]
--- Level.py
+++ Level.py
@@ -56,7 +56,7 @@ class Level():
             for x,c in enumerate(line):
                 if c in "x" :       #enemies
                     self.enemies += [Enemy("Enemy-Pew Left.png", 
-                                       [1,0],
+                                       1,
                                        [x*self.tileSize + self.tileSize/2,
                                         y*self.tileSize + self.tileSize/2],
                                        self.tileSize)
