8a8a00ffd656e0446b0cadfd7dc3b71f007b7351
StevenRayno <steven.rayno@kearsarge.org>
2017-01-12 10:42:05 -0500

    fixed now?

--- Enemy.py
+++ Enemy.py
@@ -11,10 +11,10 @@ class Enemy():
 
         self.imageDown = pygame.image.load("Resources/Enemy/Enemy-Pew Down.png"),
         
-        self.imageLeft = pygame.transform.scale(self.imageLeft, [self.size,self.size])
-        self.imageRight = pygame.transform.scale(self.imageRight, [self.size,self.size])
-        self.imageUp = pygame.transform.scale(self.imageUp, [self.size,self.size])
-        self.imageDown = pygame.transform.scale(self.imageDown, [self.size,self.size])
+        #self.imageLeft = pygame.transform.scale(self.imageLeft, [self.size,self.size])
+        #self.imageRight = pygame.transform.scale(self.imageRight, [self.size,self.size])
+        #self.imageUp = pygame.transform.scale(self.imageUp, [self.size,self.size])
+        #self.imageDown = pygame.transform.scale(self.imageDown, [self.size,self.size])
         self.image = self.imageRight
         self.rect = self.image.get_rect(center = pos)
         self.maxSpeed = speed
@@ -31,10 +31,10 @@ class Enemy():
         self.state = "right"
         self.prevState = "right"
         
-        self.frame = 0
-        self.maxFrame = len(self.images) - 1
-        self.animationTimer = 0
-        self.animationTimerMax = .3 * 60 #seconds * 60 fps
+        #self.frame = 0
+        #self.maxFrame = len(self.images) - 1
+        #self.animationTimer = 0
+        #self.animationTimerMax = .3 * 60 #seconds * 60 fps
         
         
     def move(self):
