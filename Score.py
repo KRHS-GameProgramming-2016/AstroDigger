a1099d7b2a80fb27b9ab0de3becb741c37b8ce67
StevenRayno <steven.rayno@kearsarge.org>
2017-02-03 11:01:15 -0500

    cough cough trump cough cough

--- Score.py
+++ Score.py
@@ -4,7 +4,7 @@ class Score():
     def __init__(self, pos):
         self.value = 0
         #http://www.1001fonts.com/bustin-jieber-font.html
-        self.font = pygame.font.Font("Resources/Fonts/power-pixel-7/power_pixel-7.ttf", 50)
+        self.font = pygame.font.Font("Resources/Fonts/power-pixel-7/power_pixel-7.ttf", 40)
         self.image = self.font.render("Score: " + str(self.value), True, (255,0,0))
         self.rect = self.image.get_rect(center = pos)
 
