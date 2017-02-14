742b89dc233c4e1050c86c6e8093fe1826eb8655
StevenRayno <steven.rayno@kearsarge.org>
2017-02-14 11:38:46 -0500

    new picture

--- Background.py
+++ Background.py
@@ -2,7 +2,13 @@ import math, sys, pygame
 
 class Background():
     def __init__(self, size=None):
-        self.image = pygame.image.load("Resources/Backgrounds/killmeagain.jpg")
+        self.image = pygame.image.load("Resources/Backgrounds/noplz.jpg")
         if size:
             self.image = pygame.transform.scale(self.image, size)
         self.rect = self.image.get_rect()
+#Types of Backgrounds you can choose
+#why.jpg
+#killmenow.jpg
+#killmeagain.jpg
+#killme.jpg
+#noplz.jpg
