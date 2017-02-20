f47e0a62d5290ba857536ea31c4f388a30ec30f4
Ailish <ailish.olearyaustin@kearsarge.org>
2017-02-20 11:37:20 -0500

    player respawns when loses life now

--- Player.py
+++ Player.py
@@ -178,35 +178,16 @@ class Player():
     def enemyCollide(self, other):
         if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
                 if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
-                    self.speedx = -self.speedx
-                    self.move()
-                    self.speedy = 0
-                    self.didBounceX = True
-                    if not self.didBounceY:
-                        self.speedy = -self.speedy
-                        self.move()
-                        self.speedx = 0
-                        self.didBounceX = True
-                    
-    def enemyCollide(self, other):
-        if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
-                if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
-                    diffX = self.rect.centerx - other.rect.centerx
-                    diffY = self.rect.centery - other.rect.centery
-                    if abs(diffX) > abs(diffY): #left right collide
-                        if diffX > 0: #left
-                            self.rect.left = other.rect.right + 1
-                        else:
-                            self.rect.right = other.rect.left - 1
-                        self.speedx = 0
-                        self.hit = True
-                    else:
-                        if diffY > 0: #below
-                            self.rect.top = other.rect.bottom + 1
-                        else:
-                            self.rect.bottom = other.rect.top - 1
-                        self.speedy = 0
-                        self.hit = True
+                    self.hit = True
+                    #self.speedx = -self.speedx
+                    #self.move()
+                    #self.speedy = 0
+                    #self.didBounceX = True
+                    #if not self.didBounceY:
+                        #self.speedy = -self.speedy
+                        #self.move()
+                        #self.speedx = 0
+                        #self.didBounceX = True
     
     def screenCollide(self, screenWidth):
         if self.rect.center[0] > screenWidth:
